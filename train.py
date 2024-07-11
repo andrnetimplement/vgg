05aeffc66ec61a65bb568704304e72448cbed841
Andrey Nemerov <andrey.nemerov@rt.ru>
2024-07-11 13:20:01 +0300

    обучение на cifar и на 2х картах на imagenet

--- train.py
+++ train.py
@@ -1,79 +1,88 @@
 import torch
-import torch.nn
+import torch.nn as nn
 import torch.optim as optim
-import torch.utils
-from torchvision import datasets, models, transforms
-from model import *
+import torchvision
+import torchvision.transforms as transforms
+from torchvision import models
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import ImageFolder
+from torchvision import datasets, transforms, models
+from torch.nn.parallel import DataParallel
+
 import os
 
-data_transforms = {
-    'train': transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-    'val': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ]),
-}
+data_dir = 'imagenet/ILSVRC/Data/CLS-LOC/train'
 
-data_dir = 'data/imagnet'
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
-                  for x in ['train', 'val']}
-dataloaders = {
-    x: torch.utils.data.DataLoader(image_datasets[x], batch_size=32, shuffle=True, num_workers=4)
-    for x in ['train', 'val']
-}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train','val']}
-class_names = image_datasets['train'].classes
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else "cpu")
-
-model = vgg16()
-model.to(device)
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
 
 
+dataset = torchvision.datasets.ImageFolder(data_dir, transform=transform)
+
+
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+BATCH_SIZE = 1024
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=8, pin_memory=True)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=8, pin_memory=True)
+
+num_classes = 1000
+model19 = torch.load('vgg_models/model19_cifar10.pth')
+model19.classifier[6] = nn.Linear(4096, 1000)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+if torch.cuda.device_count() > 1:
+    print(f"Using {torch.cuda.device_count()} GPUs!")
+    model19 = nn.DataParallel(model19)
+model19.to(device)
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+optimizer = torch.optim.SGD(model19.parameters(), lr=0.001, momentum=0.9)
 
-num_epoch = 25
-for epoch in range(num_epoch):
-    for phase in ['train','valid']:
-        if phase == 'train':
-            model.train()
-        else:
-            model.eval()
-    
-    running_loss = 0.0
-    running_corrects = 0
-    
-    for inputs, labels in dataloaders[phase]:
-        inputs = inputs.to(device)
-        labels = labels.to(device)
-        
-        optimizer.zero_grad()
-        
-        with torch.set_grad_enabled(phase=='train'):
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
-            loss = criterion(outputs, labels)
+def train(model, epochs):
+    for epoch in range(epochs):
+        model.train()
+        running_loss = 0.0
+        for i, (inputs, labels) in enumerate(train_loader):
+            inputs, labels = inputs.to(device), labels.to(device)
             
-            if phase == 'train':
-                loss.backward()
-                optimizer.step()
-                
-        running_loss += loss.item() * inputs.size(0)
-        running_corrects += torch.sum(preds == labels.data)
-    epoch_loss = running_loss / dataset_sizes[phase]
-    epoch_acc = running_corrects.double() / dataset_sizes[phase]
-    
-    print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
-    
-print('Training complete')
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            running_loss += loss.item()
+            if i % 100 == 99:
+                print(f'Epoch [{epoch+1}/{epochs}], Step [{i+1}/{len(train_loader)}], Loss: {running_loss/100:.4f}')
+                running_loss = 0.0
+        
+        # Оценка на тестовой выборке
+        model.eval()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for inputs, labels in test_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        
+        print(f'Accuracy on test set: {100 * correct / total:.2f}%')
+        
+train(model19, epochs=100)
 
-    
+split_indices = {
+    'train': train_dataset.indices,
+    'test': test_dataset.indices
+}
 
+# Сохранение индексов
+torch.save(split_indices, 'vgg_models/dataset_split.pt')
+torch.save(model19, 'vgg_models/model19_epoch100.pt')
---
+++ train_cifar.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torchvision import models
+from torch.utils.data import DataLoader
+
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=2)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=2)
+
+model = models.vgg16(pretrained=False)
+model.classifier[6] = nn.Linear(4096, 10)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+def train_model(model, train_loader, test_loader, criterion, optimizer, num_epochs=25):
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch+1}/{num_epochs}')
+        print('-' * 10)
+
+        # Тренировка
+        model.train()
+        running_loss = 0.0
+        running_corrects = 0
+
+        for inputs, labels in train_loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            optimizer.zero_grad()
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            loss = criterion(outputs, labels)
+
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == labels.data)
+
+        epoch_loss = running_loss / len(train_loader.dataset)
+        epoch_acc = running_corrects.double() / len(train_loader.dataset)
+
+        print(f'Train Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+        # Валидация
+        model.eval()
+        val_loss = 0.0
+        val_corrects = 0
+
+        with torch.no_grad():
+            for inputs, labels in test_loader:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                outputs = model(inputs)
+                _, preds = torch.max(outputs, 1)
+                loss = criterion(outputs, labels)
+
+                val_loss += loss.item() * inputs.size(0)
+                val_corrects += torch.sum(preds == labels.data)
+
+        val_loss = val_loss / len(test_loader.dataset)
+        val_acc = val_corrects.double() / len(test_loader.dataset)
+
+        print(f'Val Loss: {val_loss:.4f} Acc: {val_acc:.4f}')
+
+    return model
+
+model = train_model(model, train_loader, test_loader, criterion, optimizer, num_epochs=10)
+
